7b23ca7a11b59a6dad2fe24dd6f4e48748e4d64a
Freddy Hernandez <freddy@MacBook-Air-de-Freddy.local>
2026-06-25 09:39:04 -0500

    internal y export for NEE

--- R/NEE.R
+++ R/NEE.R
@@ -146,7 +146,7 @@ NEE <- function(mu.link="log", sigma.link="log") {
 #' @description This function generates initial values for the parameters.
 #' @param y vector with the response variable.
 #' @return returns a vector with the MLE estimations.
-#' @noRd
+#' @keywords internal
 #' @export
 #' @importFrom stats optim
 estim_mu_sigma_NEE <- function(y) {
@@ -165,7 +165,7 @@ estim_mu_sigma_NEE <- function(y) {
 #' @param logparam vector with parameters in log scale.
 #' @param x vector with the response variable.
 #' @return returns the loglikelihood given the parameters and random sample.
-#' @noRd
+#' @keywords internal
 #' @export
 logLik_NEE <- function(logparam=c(0, 0), x){
   return(sum(dNEE(x,
